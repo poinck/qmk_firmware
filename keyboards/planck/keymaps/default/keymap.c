69c361aab00fb9e1544e5f1b5d5d0d934a32c1e8
André <andre@poinck.de>
2017-01-14 12:19:03 +0100

    add numblock and printscreen
    - other keys had to be shifted accordingly

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -123,17 +123,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | MUTE |   1  |  2   |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   !  |   "  |   §  |   $  |   %  |   &  |   /  |   (  |   )  |   =  |      |
+ * |      |   !  |   "  |   §  |   $  |   %  |   &  |   4  |   5  |   6  |   =  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   °  |   +  |   '  |   <  |   ?  |  é   |  |   |      |      |      | <F24 |
+ * |      |   °  |   +  |   '  |   <  |   ?  |  ´   |   1  |   2  |   3  |   /  | <F24 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |  BACKLIGHT  |      |      | Vol- | Vol+ | <F23 |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
   {KC_AUDIO_MUTE, KC_1, KC_2, KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
-  {_______, KC_EXLM, S(KC_2), S(KC_3), KC_DLR,  KC_PERC, S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______},
-  {KC_LOCKING_CAPS,S(KC_GRAVE),KC_RBRC,S(KC_BSLS),KC_NUBS,S(KC_MINS),KC_EQL,RALT(KC_NUBS),_______,_______,_______,KC_F24},
+  {_______, KC_EXLM, S(KC_2), S(KC_3), KC_DLR,  KC_PERC, S(KC_6), KC_4,    KC_5,    KC_6, S(KC_0), _______},
+  {_______,S(KC_GRAVE),KC_RBRC,S(KC_BSLS),KC_NUBS,S(KC_MINS),KC_EQL,KC_1,  KC_2,    KC_3, S(KC_7),    KC_F24},
   {_______, _______, _______, _______, _______, BACKLIT, BACKLIT, _______, _______, KC_VOLD, KC_VOLU, KC_F23}
 },
 /* old-lower-default
@@ -160,17 +160,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ^  | <F1  | <F2> | <F3> | <F4> | <F5> | <F6> | <F7> | <F8> | <F9  | <F10>| Ins  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |  ~   |  {   |  [   |  ]   |  }   |      |
+ * | Stop | Prev | Play | Next |  (   |  )   |  ~   |  {   |  [   |  ]   |  }   |Print |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  ^   |  *   |  #   |  |   |  \   |      |  >   |  ü   |  ö   |  ä   | PgUp |
+ * |      |  ^   |  *   |  #   |  |   |  /   |  `   |  >   |  ü   |  ö   |  ä   | PgUp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |  BAVKLIGHT  |      |      | Pos1 | Ende |PgDown|
+ * |      |      |      |      |      |  BACKLIGHT  |      |      | Pos1 | Ende |PgDown|
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
   {KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_INS},
-  {KC_MSTP,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,RALT(KC_RBRC),RALT(KC_7),RALT(KC_8),RALT(KC_9),RALT(KC_0),_______},
-  {_______,KC_GRV,S(KC_RBRC),KC_BSLS,RALT(KC_NUBS),RALT(KC_MINS),S(KC_EQL),S(KC_NUBS),KC_LBRC,KC_SCLN,KC_QUOT,KC_PGUP},
+  {KC_MSTP,KC_MPRV,KC_MPLY,KC_MNXT,S(KC_8),S(KC_9),RALT(KC_RBRC),RALT(KC_7),RALT(KC_8),RALT(KC_9),RALT(KC_0),KC_PSCR},
+  {_______,KC_GRV,S(KC_RBRC),KC_BSLS,RALT(KC_NUBS),S(KC_7),S(KC_EQL),S(KC_NUBS),KC_LBRC,KC_SCLN,KC_QUOT,KC_PGUP},
   {_______, _______, _______, _______, _______, BACKLIT, BACKLIT, _______, _______, KC_HOME, KC_END,  KC_PGDOWN}
 },
 /* old-raise-default
@@ -220,9 +220,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 
 [_SUPERLOW] = {
-  {KC_ESC, LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9),LGUI(KC_0),KC_BSPC},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______,_______,_______,LGUI(KC_UP)},
+  {KC_ESC,LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9),LGUI(KC_0),KC_BSPC},
+  {_______,_______,_______,_______,_______,_______,_______,LGUI(KC_4),LGUI(KC_5),LGUI(KC_6),_______, _______},
+  {_______,_______,_______,_______,_______,_______,_______,LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),_______,LGUI(KC_UP)},
   {_______,_______,_______,_______,_______,_______,_______,_______,_______,LGUI(KC_LEFT),LGUI(KC_RIGHT),LGUI(KC_DOWN)}
 }
 
